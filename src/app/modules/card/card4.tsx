4cfb9413f6dccb76bd06a0e6357f7490ad0bda14
avdhendrasingh <avdhendrasingh3@gmail.com>
2025-02-01 03:43:30 +0530

    frontend:momoseller

--- src/app/modules/card/card4.tsx
+++ src/app/modules/card/card4.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
 import React from 'react'
 
 type Props = {}
 
 export default function Card4({}: Props) {
   return (
-      <Image/>
+      <div>card 4</div>
   )
 }
